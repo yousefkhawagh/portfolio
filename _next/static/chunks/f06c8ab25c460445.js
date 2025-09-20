b6fafec139e8a75613d7633ff93e4553c79d81ad
yousefkhawagh <yashraf540@gmail.com>
2025-09-20 20:29:34 +0300

    Updates

---
+++ _next/static/chunks/f06c8ab25c460445.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/903351248cf37f5c.js",
+  "static/chunks/turbopack-88223e863dc999a6.js"
+])
---
+++ _next/static/chunks/f46c5935d0fe74d1.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/903351248cf37f5c.js",
+  "static/chunks/turbopack-3cf1513c3704e06c.js"
+])
