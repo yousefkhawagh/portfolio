9b64c4088e3454e530f19f639c5bcf21968da47d
yousefkhawagh <yashraf540@gmail.com>
2025-09-20 20:33:20 +0300

    Update portfolio

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",       // Tells Next.js to generate static HTML
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
